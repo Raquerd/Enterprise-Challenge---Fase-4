ab7d1c44c15efdecd1f25bd5935490ba23a15b15
Davi Ferreira <davistudyreference@gmail.com>
2025-10-01 23:33:07 -0300

    MOD: Alteração API FLASK, inserido conexão com BD Oracle Fiap

--- db/script/api.py
+++ db/script/api.py
@@ -1,16 +1,16 @@
-from flask import Flask, request, jsonify, datetime
+from flask import Flask, request, jsonify, datetime, oracledb
 
-def registrar_no_banco(temperatura, vibracao, corrente, id_maquina=101):
+def registrar_no_banco(temperatura, vibracao, corrente, id_sistema):
     """Função para inserir os dados recebidos no banco de dados."""
     try:
-        conn = sqlite3.connect(DB_FILE)
+        conn = oracledb.connect(user="rm562274", password="090402", dsn='oracle.fiap.com.br:1521/ORCL')
         cursor = conn.cursor()
         
         timestamp = datetime.datetime.now().isoformat()
         
         # Query SQL para inserir uma nova leitura
-        query = "INSERT INTO leituras_sensores (timestamp, temperatura, vibracao, corrente, id_maquina) VALUES (?, ?, ?, ?, ?)"
-        cursor.execute(query, (timestamp, temperatura, vibracao, corrente, id_maquina))
+        query = "INSERT INTO FT_REG_SENSORES (TIMESTAMP_REGISTRO, VL_TEMPERATURA, VL_VIBRACAO, VL_CORRENTE, ID_SISTEMA) VALUES (:1, :2, :3, :4, :5)"
+        cursor.execute(query, (timestamp, temperatura, vibracao, corrente, id_sistema))
         
         conn.commit()
         conn.close()
@@ -35,16 +35,16 @@ def receber_leitura():
     temp = data.get('tempC')
     current = data.get('currentA')
     vibracao = data.get('vibracao_aprox')
-    id_sensor = data.get('id_sensor')
+    id_sistema = data.get('id_sistema')
     # acc_X = data.get('accX_g')
     # acc_Y = data.get('accY_g')
     # acc_Z = data.get('accZ_g')
 
-    if temp is None or current is None or vibracao is None:
+    if temp is None or current is None or vibracao is None or id_sistema is None:
         return jsonify({"status": "erro", "mensagem": "Dados incompletos"}), 400
 
     
-    print(f"Dados recebidos: Temp={temp}, Corrente={current}, Vibração={vibracao}")
+    print(f"Dados recebidos do sistema de id {id_sistema}: Temp={temp}, Corrente={current}, Vibração={vibracao}")
 
 
 if __name__ == '__main__':
